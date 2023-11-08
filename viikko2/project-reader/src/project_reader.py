44bc8111cd42e18957a2b7763808afd00e4c12f9
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-11-08 16:00:08 +0200

    cleaned up directory structure

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,17 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        content = toml.loads(content, _dict=dict)
+        print(content)
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project("Test name", "Test description", [], [])
