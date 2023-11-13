11ffeaa695d15811663578c95f99881fdb5d0fca
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-11-13 18:40:53 +0200

    tehtävä 11

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -5,6 +5,7 @@ class Player:
         self.goals = dict['goals']
         self.assists = dict['assists']
         self.nationality = dict['nationality']
+
     
     def __str__(self):
-        return (f"{self.name} team {self.team} goals {self.goals} assists {self.assists}")
+        return (f"{self.name:20} {self.team:4} {self.goals:4} + {self.assists:4} = {self.assists + self.goals} ")
