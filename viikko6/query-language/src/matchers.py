9cf56ac2be01179592479201271f06b06f87f16c
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-12-11 20:12:21 +0200

    viikko 6 tehtävä 2

---
+++ viikko6/query-language/src/matchers.py
@@ -0,0 +1,65 @@
+class And:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                return False
+
+        return True
+
+
+class PlaysIn:
+    def __init__(self, team):
+        self._team = team
+
+    def test(self, player):
+        return player.team == self._team
+
+
+class HasAtLeast:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value >= self._value
+
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def test(self, player):
+        if self._matcher.test(player) == False:
+            return True
+        else:
+            return False
+
+
+class All:
+    def __init__(self, matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player) == True:
+                continue
+            else:
+                return False
+        return True
+
+
+class HasFewerThan:
+    def __init__(self, matcher, value):
+        self._matcher = matcher
+        self._value = value
+
+        def test(self, player):
+            if self._matcher.test(player) < self._value:
+                return True
+            else:
+                return False
